2ec6904d10530508d3b813df9ab371f75fd6210c
Antonio D. G. Medina <antgarmed@outlook.com>
2025-07-19 12:10:01 +0200

    feat: initialize backend tools project with Gradle setup and basic Spring Boot application structure

---
+++ src/main/java/com/example/backendtools/BackendToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendtools/BackendToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
